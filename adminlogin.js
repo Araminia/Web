420d2f5afdc24087103a14198edf8222c72474b9
Araminia <rosascarmina11@gmail.com>
2025-04-15 07:09:45 +0800

    adminlogin.js

--- adminlogin.js
+++ adminlogin.js
@@ -1,10 +1,14 @@
+// login.js
 function login() {
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
-
-    if (username === "01062023" && password === "02282023") {
-        window.location.href = "utot.html";
+  
+    if (username === "OMPSA" && password === "Website") {
+        window.location.href = "userinterface.html";
     } else {
-        alert("Bakit hindi mo alam? May iba ka bang ka-anniv? Ok? Ulitin mo 'yan!");
+      alert("Try again!");
     }
-}
+  }
+  
+  // Expose the login function to the global scope
+  window.login = login;
